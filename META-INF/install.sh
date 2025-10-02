b5f689286a7e4365db8189132980c5c536d70172
aum47 <134261534+aum47@users.noreply.github.com>
2025-10-02 17:53:31 +0530

    Delete META-INF directory

--- META-INF/install.sh
+++
@@ -1,5 +0,0 @@
-#!/system/bin/sh
-# This runs at install time in Magisk
-ui_print "Installing remount module..."
-cp -a common/post-fs-data.sh $MODPATH/post-fs-data.sh
-chmod 755 $MODPATH/post-fs-data.sh
